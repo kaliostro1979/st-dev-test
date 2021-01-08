2cc1143ce9043efb742f718121e015e88e401cb4
Misha <abachyanm@gmail.com>
2021-01-09 01:52:45 +0400

    All sections finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+/*Header Carousel*/
+
 let prevButton = '<img src="./assets/images/icons/nav-left.png" alt="prev">';
 let nextButton = '<img src="./assets/images/icons/nav-right.png" alt="next">';
 
@@ -9,4 +11,22 @@ $(".header-banner-slider").owlCarousel({
         prevButton,
         nextButton
     ]
+});
+
+
+/*Partners section carousel*/
+
+let prevPartButton = '<img src="./assets/images/icons/nav-left-white.png" alt="prev">';
+let nextPartButton = '<img src="./assets/images/icons/nav-right-white.png" alt="next">';
+
+$(".partners-slider").owlCarousel({
+    items: 4,
+    dots: false,
+    nav: true,
+    navText: [
+        prevPartButton,
+        nextPartButton
+    ],
+    margin: 36,
+    stagePadding: 36
 });
